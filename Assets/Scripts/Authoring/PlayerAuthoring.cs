8083bf4c06a9c8fb5e9e347449c0d337582ea142
Kacper Gąsior <kacper.gasior@themomentum.ai>
2026-07-16 22:18:18 +0200

    feat: add player entity authoring and baking

---
+++ Assets/Scripts/Authoring/PlayerAuthoring.cs
@@ -0,0 +1,24 @@
+using Unity.Entities;
+using UnityEngine;
+
+public class PlayerAuthoring : MonoBehaviour
+{
+    public int playerId;
+
+    public class Baker : Baker<PlayerAuthoring>
+    {
+        public override void Bake(PlayerAuthoring authoring)
+        {
+            Entity entity = GetEntity(
+                TransformUsageFlags.Dynamic
+            );
+
+            AddComponent<PlayerTag>(entity);
+
+            AddComponent(entity, new PlayerId
+            {
+                Value = authoring.playerId
+            });
+        }
+    }
+}
---
+++ Assets/Scripts/Components/PlayerId.cs
@@ -0,0 +1,6 @@
+using Unity.Entities;
+
+public struct PlayerId : IComponentData
+{
+    public int Value;
+}
